ef9728d049117068832020a396024fafde93c862
Laura Kutschan <laurakutschan@Lauras-MacBook-Pro.local>
2021-12-20 17:04:37 +0100

    design update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LayoutComponent} from "./layout/layout.component";
+import {CrudComponent} from "./crud/crud.component";
+import {ContactComponent} from "./contact/contact.component";
+import {LikeComponent} from "./like/like.component";
+import {ShareComponent} from "./share/share.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: "pictures",
+    component: CrudComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+  {
+    path: "like",
+    component: LikeComponent
+  },
+  {
+    path: "share",
+    component: ShareComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { CrudComponent } from './crud/crud.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSelectModule} from "@angular/material/select";
+import {RouterModule} from "@angular/router";
+import { ShareComponent } from './share/share.component';
+import { LikeComponent } from './like/like.component';
+import { ContactComponent } from './contact/contact.component';
+import {MatStepperModule} from "@angular/material/stepper";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatCardModule} from "@angular/material/card";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    CrudComponent,
+    ShareComponent,
+    LikeComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
